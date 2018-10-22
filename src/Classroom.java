7ce8a1072133d20a436d73be77dcfbb14f96aabc
BT_1N3_16 <zchen5143@bths.edu>
2018-10-22 14:47:46 -0400

    CLASSROOM!

---
+++ src/Classroom.java
@@ -0,0 +1,20 @@
+public class Classroom {
+    private Student[] students;
+    private Teacher teacher;
+    public Classroom (Teacher teacher, Student[] students)
+    {
+        this.students= students;
+        this.teacher=teacher;
+    }
+    public void getSubject()
+    {
+        return Subject;
+    }
+    public double classAverage()
+    {
+        for (double val : students)
+      {
+         double totalGPA =
+      }
+    }
+}
