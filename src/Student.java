b121f16965abd2ceef7a3ee1d9c1628678bb076d
Abdul Rafi <arafi6624@bths.edu>
2018-10-22 14:48:50 -0400

    Student class created

---
+++ src/Student.java
@@ -0,0 +1,18 @@
+public class Student extends Person {
+    private double GPA;
+    private int osis;
+    private int gradeNum;
+
+    public Student (double GPA, int osis, int gradeNum, String firstName, String familyName){
+        super (firstName, familyName);
+        this.GPA = GPA;
+        this.osis = osis;
+        this.gradeNum = gradeNum;
+    }
+
+    public double getGPA(){
+        return GPA;
+    };
+
+    public String toString(return )
+}
